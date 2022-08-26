3c43f68f1aa322d73761cb72bcda05aa4b27f648
ManiMaran00 <112166256+ManiMaran00@users.noreply.github.com>
2022-08-26 16:19:16 +0530

    Add files via upload

---
+++ EmplpoyeeDetails.java
@@ -0,0 +1,15 @@
+package EmployeeDetails;
+public class EmplpoyeeDetails {
+		private void empName() {
+			System.out.print("Mani");		
+		}
+		private void empMobile() {
+			System.out.print("9003966598");		
+		}
+		public static void main(String[] args) {
+			EmplpoyeeDetails e = new EmplpoyeeDetails();
+			e.empMobile();
+			e.empName();
+			
+		}
+}
